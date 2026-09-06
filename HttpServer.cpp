02ce38a421b1cf28dbda142bbcdbe67eb4feb720
nandom73-sketch <nandom73@gmail.com>
2026-09-06 21:48:39 +0200

    Add VMC HTTP control API

--- HttpServer.cpp
+++ HttpServer.cpp
@@ -5,10 +5,12 @@
 #include "HttpServer.h"
 #include "Climate.h"
 #include "Fans.h"
+#include "Vmc.h"
 
 #include <Arduino.h>
 #include <WebServer.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 
 namespace
@@ -50,11 +52,87 @@ void handleRoot()
 
     server.send(200, "text/html", htmlBuffer);
 }
+
+const char* vmcModeToString(VmcMode mode)
+{
+    switch (mode)
+    {
+        case VmcMode::OFF:
+            return "OFF";
+        case VmcMode::MANUALE:
+            return "MANUALE";
+        case VmcMode::BOOST:
+            return "BOOST";
+    }
+
+    return "OFF";
+}
+
+void sendVmcStatus()
+{
+    const VmcData& vmc = Vmc_getData();
+
+    snprintf(htmlBuffer,
+             sizeof(htmlBuffer),
+             "{\"mode\":\"%s\",\"speed\":%u,\"boostRemainingSeconds\":%lu}",
+             vmcModeToString(vmc.mode),
+             vmc.speed,
+             static_cast<unsigned long>(vmc.boostRemainingSeconds));
+
+    server.send(200, "application/json", htmlBuffer);
+}
+
+void sendBadRequest(const char* message)
+{
+    snprintf(htmlBuffer,
+             sizeof(htmlBuffer),
+             "{\"error\":\"%s\"}",
+             message);
+    server.send(400, "application/json", htmlBuffer);
+}
+
+void handleSetSpeed()
+{
+    if (!server.hasArg("speed"))
+    {
+        sendBadRequest("Missing speed parameter");
+        return;
+    }
+
+    const String speedArgument = server.arg("speed");
+    char* end = nullptr;
+    const long speed = strtol(speedArgument.c_str(), &end, 10);
+
+    if (speedArgument.length() == 0 || *end != '\0' || speed < 0 || speed > 7)
+    {
+        sendBadRequest("Speed must be an integer between 0 and 7");
+        return;
+    }
+
+    Vmc_setManualSpeed(static_cast<uint8_t>(speed));
+    sendVmcStatus();
+}
+
+void handleStartBoost()
+{
+    Vmc_startBoost();
+    sendVmcStatus();
+}
+
+void handleCancelBoost()
+{
+    Vmc_cancelBoost();
+    sendVmcStatus();
+}
 } // namespace
 
 bool WebServer_begin()
 {
     server.on("/", handleRoot);
+    server.on("/api/status", HTTP_GET, sendVmcStatus);
+    server.on("/api/speed", HTTP_POST, handleSetSpeed);
+    server.on("/api/boost", HTTP_POST, handleStartBoost);
+    server.on("/api/boost/cancel", HTTP_POST, handleCancelBoost);
     server.begin();
 
     return true;
